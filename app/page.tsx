9b2bab54033c44011ecfea131ec6b0eee8603f2a
abhinav-khare-882 <abhinavkhare882@gmail.com>
2024-01-28 14:25:25 +0530

    tailwind config and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div>Hi there</div>;
+  return (
+    <div>
+      <h1 className="h1-bold">hello user</h1>
+      <h2 className="h2-bold">hello user from another place</h2>
+      <h3 className="h3-bold">you're not welcomed here</h3>
+    </div>
+  );
 }
