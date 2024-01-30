9d33843118d8b6d1e0bf53fcb757f33c4d879762
abhinav-khare-882 <abhinavkhare882@gmail.com>
2024-01-30 15:01:15 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">hello user</h1>
-      <h2 className="h2-bold">hello user from another place</h2>
-      <h3 className="h3-bold">you're not welcomed here</h3>
-    </div>
-  );
-}
